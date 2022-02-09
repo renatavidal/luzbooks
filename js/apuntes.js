18714c2d5e6f501d47ea6ebeac5e3c59758f777c
rena <renata.vidal.19@hotmail.com>
2022-02-09 20:24:36 -0300

    target

--- js/apuntes.js
+++ js/apuntes.js
@@ -336,4 +336,379 @@ CREAR E INSERTAR TEXTO EN EL DOM
             const fragment = document.createDocumentFragment() par ameter todo el codigo de golpe 
             ahi puedo hacer un for metiendo dias por ejemplo y en el fragment voy a;adiendolo con appendChild y recien cuando termino de agregar todo lo que quiero es introducir en ,por ejemplo, dayList.appendChild(fragment)
 
-            
+DOM TRAVERSING
+        RECORRER EL NODO
+        PADRE 
+            parentNode devuelve el nodo padre que puede no ser un elemento
+            parentElement devuevle el nodo elemeneto padre si hago parentNode.parentNode me da el abuelo y asi consecutivamente.
+            los noods de tipo element y element fragment nunca tendran nodo padre 
+        HIJOS 
+            childNodes todos los nodos hijos 
+            children todos los nodos elementos hijos 
+            firstChild primer hijo 
+            firstElementChild primer elemento hijo 
+            lastChild 
+            lastElementChild 
+            hasChildNodes() true si el nodo tiene hijos y false de lo contrario
+        HERMANOS 
+            nextSibiling siguiente nodo hermano 
+            nextElementSibiling 
+            previousSibiling 
+            previousElementSibiling 
+        CERCANO 
+            closest(selector) selecciona el nodo mas cercano que cumpla con el selector (aun es experimental).
+    INSERTAR, CLONAR Y BORRAR ELEMENTOS 
+        Por ejemplo append child lo mete siempre ultimo por eso tenemos: 
+        parent.insertBefore(newelement, referenceelement)
+
+        parent.insertAdjacentElement(position,element)
+        parent.insertAdjacentHTML(position,HTML) estos dos funcionan igual pero con por ejemplo '<li> nuevo elemento </li>' (en ambos, el segundo parametro)
+        parent.insertAdjacentText(position,text)
+
+        positions: 
+        beforebegin antes de que empiece (hermano anterior) van asi 'beforebegin'
+        afterbegin despuesd e que empiece (primer hijo)
+        beforeend antes e que termine (ultimo hijo)
+        afterend despues de qeu acabe (hermano siguiente)
+        
+        parent.replaceChild(newChild, oldchild)
+
+        ESTOS NO FUNCIONAN EN INTERNET EXPLORER
+
+            parent.before() hermano anterior 
+            parent.prepend() primer hijo 
+            parent.append() ultimo hijo 
+            parent.after() hermano siguiente
+            child.replaaceWith(newChild)
+
+            clonar y eliminar 
+            element.cloneNode(true|false) clona el nodo. true: todo el elemento con los hijos, false: elemento sin hijos
+            element.remove()
+            element.removeChild(child)
+    OBJETOS NATIVOS Y TIMERS 
+        objeto window objeto global de el descienden todos los objetos, siempre que no ponemos nada adelante el navegador va a dar por hecho que es window.
+        NO ESTA BUENO ESTO PORQUE FRENA LA CARGA DE LA PAGINA
+        alert() imprimir cosas
+        promt() para que el usuario escriba
+        confirm() es para aceptar o cancelar, es booleano. no se le puede dar estilo a esa ventana.
+        objeto console- el objeto qeu contienen la consola del navegador
+        console.log() es el que imprime por consola
+        console.dir() parecido al anterior pero siempre se puede desplegar (usar si el otro no despliega por ejemplo un ul (lista))
+        console.error() salta un error por consola
+        console.table() te muestra el elemento que le pongas pero en una tabla toda canchera por consola
+
+        objeto location objeto que contiene la barra de direcciones 
+        locaiton. 
+        href la dire del lugar, podemos cambiar al valor de la url
+        protocol a ver si es http o qeu cosa 
+        host dominio principal que estemos visualizando 
+        pathname el resto del coso, el directorio 
+        hash paramtros entre paginas 
+        reload() recarga la pagina cada vez que lo lee, se suele poner en un if por si pasa algo 
+
+        objeto history - historial del navegador
+        back() una pagina hacia atras
+        foward() una pagina hacia adelante
+        go(n|-n) o negativo o positivo,  n pag hacia adelante o -n paginas hacia atras
+        lenght  cantidad d e paginas en el historial 
+
+        objeto date 
+        tiene un monton de metodos que estan en internet je 
+        necesita ser instanciado osea inicializado
+        const date= new Date()
+        console.log(date.getDay()) y ahi te tira que dia es (empieza a contar desde 0 osea domingo es 0)
+
+    timers 
+        timeout
+        setTimeout(()=>{code},delay-in-miliseconds) hace que se ejecute la funcon despues del delay. si lo referenciamos mediante una variable/constante podemos pararlo con clearTimeout(referencia)
+        interval
+        setInterval(()=>{code},delay-in-miliseconds) hace que se ejecute la funcion cada delay de milisegundos. si lo referenciamos mediante una variable/constante podemos pararlo con clearInterval(referencia)
+        ejemplos:
+            button.addEventListener('click',()=>{
+                setTimeout(saludar, 3000);
+            )}
+            const saludar= () =>{
+                console.log('hola');
+            }
+
+            o asi 
+            button.addEventListener('click',()=>{
+                setTimeout(() =>{
+                    console.log('hola');}
+                    , 3000)
+            )}
+            para pararlo con celarTimout tengo que guardar el timout en una variable
+            const timeout= setTimeout(() =>{
+                console.log('hola');}
+                , 3000)
+            )}
+            button.addEventListener('click',()=>{
+                clearTimeout(timeout);
+            }) aca se ejecutaria el holi a menos que haga click en el boton antes de los 3 segundos
+
+OBJETO XMLHTTP REQUEST 
+            AJAX 
+            se suele hacer pho las pediciones a los servidores 
+            button.addEventListener('click', ()=>{
+                ler xhr
+                if (window.XMLHttpRequest) //aca le digoque el navegador lo soporte
+                    xhr= new XMLHttpREquest()
+                } else {
+                    xhr= new ActiveXObject("Microsoft.XMLHTTP")
+                    
+                }
+                xhr.open('GET', ' direccion del archivo php o url')// el usa el url de json place holder
+                xhr.addEventListener('load', (data)=>{
+                    const dataJSON = JSON.parce(data.taget.response); // java script object notation, osea convierto el string en objeto javascript
+                    const list= document.getElementById('list'); //ese id es de HMTL
+                    for( const userinfro of dataJSON){
+                        const listItem= document.createElement('LI')
+                        listItem.textContent= `${userInfo.id} - ${userInfo.name}`
+                        list.appendChild(listItem);
+                    }
+                }) //esto hace que sepamos que la info esta terminada y guardar la info en una variabole 
+                xhr.send()
+            }) //con esto nos ahorramos jquery, no se pq 
+
+
+
+
+CALLBACKS 
+            es una funcion que se ejecuta a traves de una funcion
+            no son asincronos es decir se ejecutan por orden 
+            ejemplo 
+            const getEmail= (user,cb)=> {
+                const email = emails.find(email => email.id == user.id)
+                if (!email) cb (`${user.name} hasnt got email`)
+                else cb (null, { 
+                    id: user.id,
+                    name: user.name,
+                    email: email.email
+                })
+            }
+            getEmail(3, (err,res)=>{
+                if(err) return console.log(err)
+                console.log(res);
+            })
+
+PROMESAS 
+            objeto que dentro tiene cdos callbacks internos 
+            const promise = new Promise((resolve, reject)) esos callbacks vienen preprogramados
+             por ej
+             const getUSer = (id)=>{
+                const user = users.find(user=>user.id == id);
+                return promise = new Promise((resolve, reject)=>{
+                    if(!user) reject('no existe');
+                    else resolve(user);
+                )}
+                }
+             ahora leemose lecontenido de las promedas 
+             getUser(1)
+                .then(user => getEmail(user)) //otra funcion
+                .then(res=> console.log(res)) // imptimo la respuesta de getEmail
+                .catch(err=> console.log(err)) //este catch agarra cualquier error de la ejeucion, incluso los de getEmail 
+
+FETCH reemplazo modermo para el objetO XMLHttpRequest 
+                encapsula y manipula promesas - se suele usar para hacerpeticiones a API 
+                tiene response y reject internos 
+                arrayBuffer() archivos binarios en bruto (mp3,pdf,jpg,etc) se utiliza cuando se necesita manipular el contenido del archivo
+                blob() archivos binarios en burto, se utiliza cuando no se necesita manipular el contenido y se va a trabajar con el archivo directamente 
+                clone() creo clon de un objeto de respuesta, identro con todos los setnidos pero almacenado en una varibale diferente 
+                formData() se utiliza para leer los objetos formData 
+                json() convierte archivos json en un objeto de javascript 
+                text() se utiliza cuando queremos leer un archivo de texto. siempre se codifica en UTF-8 
+        COMPROBAR EL SOPORTE FETCH 
+                if( window.fetch != undefined) console.log('anda')
+                else console.log('no anda')
+
+                const button= document.getElementById('button')
+                button.addEventListener('click', ()=>{
+                    fetch('url')
+                        .then(res ==> res.ok ? Promise.resolve(res) : Promise.reject(res))
+                        .then( res=> res.json) // aca llega si hay un resolve por parte de fetch. res esta encapsulado entonces tengo que hacerlo json
+                        .then(res => {
+                            const list= document.getElementById('list')
+                            const fragment= document.createDocumentFragment()
+                            for (const userInfo of res){
+                                const listItem = document.createElement('LI')
+                                listItem.textContent = `${userInfo.id} - ${userInfo.name}`
+                                fragment.appendChild(listItem);
+                            }
+                            list.appendChild(fragment)
+                        }
+                    .catch( err => console.log(err))
+                })
+        PETICIONES POST CON FETCH 
+                fetch(url, {
+                    method: 'POST',
+                    body: los daot suqe enviamos, si es un objeto hay que convertirlo con JSON.stirngtify(datos)
+                    headers: {
+                        cabeceras de info sobre lo que enviamos
+                    }
+                })
+
+                ejemplo 
+                const button= document.getElementById('button')
+                button.addEventListener('click', ()=>{
+                    const newPost = {
+                        title: 'new post'
+                        body: ' loremmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm',
+                        userId:1
+                    }
+                    fetch('url', {
+                        method:'POST'
+                        body: JSON.stringtify(newPost),
+                        headers:{
+                            "content-type": " application/json</>"
+                    }).then(res=> res.json())
+                    .then(data => console.log(data))
+                })
+        FETCH LECTURA DE ARCHIVOS 
+        const buttonIMG= document.getElementById('button-img')
+        const buttonPDF= document.getElementById('button-pdf')
+        buttonIMG.addEventListener('click', ()=>{
+            fetch('dog.jpg')
+                .then(res=> res.blob()) // blob es binary long object
+                .then( img=> {
+                    document.getElementById('img').src = URL.createObjectURL(img) //consvierte el objeto en una url valida par visualizar el objeto, tiene que ser blob
+                })
+                //para pdf es lo mismo cambiando el img por pdf y el src por href 
+ASYNC AWAIT 
+            hacer funciones asincronas en js de forma nativa 
+            funciona con promesas
+
+            const getName = async() => {
+                return new Promise((resolve, reject)=>{
+                    setTimeout(()=>{
+                        resolve ('Dorian')
+                    }, 1500)
+                    
+                })
+            }
+
+            const sayHello = async()=> {
+                const name = await getName() //tengo que ponerle ese await, que esta siempre en async, para avisarle que va a tardar ese name en llegar
+                return `Hello ${name}`
+            }
+            console.log(sayHello()) //esto va a decir hello promise o algo asi, para solucionar hago asi 
+
+            sayHello().then(res=>console.log(res)) 
+
+LIBRERIA AXIOS
+            libreria basada en promesas para el cliente y el servidor 
+            tiene transformacion automatica cuando hacemos peticiones json 
+            tiene soporte para internet explorer 11 
+            pesa poco y hace peticiones en muy poco tiempo 
+            hay que insertarla en un <script url= "plinplinplin"></script> en el html
+        GET  
+            ej 
+            buttonIMG.addEventListener('click', ()=>{
+                axios({
+                    method: 'GET',
+                    url: "url"
+                }).then (res =>{
+                    const list= document.getElementById('list')
+                    const fragment= document.createDocumentFragment()
+                    for (const userInfo of res.data){
+                        const listItem = document.createElement('LI')
+                        listItem.textContent = `${userInfo.id} - ${userInfo.name}`
+                        fragment.appendChild(listItem);
+                    }
+                    list.appendChild(fragment)
+                }).cathc(err =>console.log(err))
+            })
+
+
+        POST
+        buttonIMG.addEventListener('click', ()=>{
+            axios({
+                method: 'POST',
+                url: "url"
+                data:{
+                    title: 'new post'
+                    body: ' loremmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm',
+                    userId:1
+                }
+            })
+            .then (res => console.log(res.data)
+            .cathc(err =>console.log(err))
+        })
+
+
+FORMULARIOS
+        validar formularios 
+        supongo que tengo todas las variables guardadas con el get element by id 
+        ahora pongo 
+        const forIsValid = {
+            name:false,
+            email: false,
+            gender: false,
+            terms: false,
+        }
+        form.addEventListener('sumbit', (e)=> {
+            e.preventDefault() //para que la persona no pueda activar el boton desde inspeccionar 
+        })
+        name.addEventListener('change', (e)=>{
+            if(e.target.value.trim().lenght>0) formIsValid.name=true;
+        })
+        //y asi con todos
+        y para hacer por ejemplo que si acepta los terminos y condiciones se ponga en true hago asi 
+        terms.addEventListener('change', (e)=>{
+            forIsValid.terms= e.target.checked
+            e.target.checked ? button.removeAttribute('disabled'): 
+            button.setAttribute('disabled', true); //este button los get by id
+        })
+        const validateForm = ()=> {
+            const formValues= object.values(formIsvalid)
+            const valid = formValues.findIndex( value => value == false)
+            if(valid == -1) form.submit()
+            else alert('form invalid')
+        }
+    EXPRESIONES REGULARES 
+        secuencia de caracteres que busca un patron de busqueda 
+        sintaxis:
+        /patron/
+        banderas:
+            i: ignore case, no difiera mayusculas y minusculas 
+            g: global, no se para despues de la primera coincidencia 
+
+        ej:
+            const text= document.getElementById('text').textContent
+            const regEx= /lorem/gi //3 formas de hacer lo mismo
+            const regEx= new REgExp ('lorem', 'gi')
+            const regEx= new REgExp ('/lorem/', 'gi')
+            console.log(regEx.text(text)) //eso nos da true porque el texto tiene lorem y aunque sea Lorem con mayuscula la i hace que no importe, se puede hacer tambien con text.includes('Lorem') pero despues si sirven las expresiones regulares 
+
+        comodines:
+            sustitucion: define un comodin dentro del patron, el simbolo en el ".", por ej i..um te encuentra el ipsum 
+            listado de caracteres validos: entre corchetes se pone una lista de caracteres validos [aeiou] ahi van todas las vocales 
+            rangos: entre corchetes [a-z] todas las letras minusculas 
+            mezcla entre rangos y listas: 
+            [0-50u] ahi van los num del o al 5 y las letras o u 
+            cadenas completas : entre parentesis, si queremos mas palabreas las separamos por | osea (lorem|amet) o (12|42) porque si pongo (1242) me busca el 1 el 2 el 4 y el 2, no el doce yel cuarenta y dos 
+
+        delimitadores:
+        ^ antes de este simbolo no puede haber nada 
+        $ despues de este simbolo no puede haber nada
+        osea ^hola$ va a dar false si tengo -hola! pero true si tengo hola 
+        cantidad:
+        {} aparecer cantidad exacta de veces :
+            {n} se repite n veces 
+            {n,m} entre n y m veces ambas incluidas 
+            {n,} como minimo n veces y sin maximo
+        * lo que esta antes de asterisco puede estar, no estar o repetir  
+            por ej [0-9]*a puede ser 034432423423a y va a ser valido pero a23423a y no por la primera a 
+        ? antes de ? puede no estar o estar solo una vez 
+        + antes del + tiene que estar una vez minimo 
+
+        caracteres:
+            \s coincide con un caracter de espacio, entre ellos incluidos estacio, tab, salto de pagina, salto de linea y retorno de carro
+
+            \S coincide con todos menos caracteres de espacio 
+            \d coincide con un caracter de numero, equivalente por ej a [0-9]
+            \D coincide con cualquier caracter no numerico 
+            \w coincide con cualquier caracter alfanumerico incluyendo el guion bajo 
+            \W coincide con todo menos caracteres de palabra por ejemplo .- ! ? 
+    
+    LIBRERIA DE VALIDACIONES 
+        <script src="validate.js"></script> y ahi te tira todas las cosillas para validar, todas las funciones hay que descargarla de github 
--- scripts.js
+++ scripts.js
@@ -23,62 +23,16 @@ button5.addEventListener('click',()=>{
 function desplegar(checkBox) {
     const text = document.getElementById('text');
     console.log(checkBox.checked)
-    console.log('hola')
     if (checkBox.checked == true){
-        console.log('holi')
       text.style.display = "block";
     } else {
       text.style.display = "none";
     }
 } 
-//  var wow = new WOW(
-//     {
-//       boxClass:     'wow',      
-//       animateClass: 'animated', 
-//       offset:       0,         
-//       mobile:       true,      
-//       live:         true,       
-//       callback:     function(box) {
-//       },
-//       scrollContainer: null 
-//     }
-//   );
-//   wow.init();
 
-
-//   <script>
-// const button1= document.getElementById('myCheck');
-// button1.addEventListener('click',myFunction(button1));
-// const button2= document.getElementById('myCheck2');
-// button2.addEventListener('click',myFunction(button2));
-// const button3= document.getElementById('myCheck3');
-// button3.addEventListener('click',myFunction(button3));
-// const button4= document.getElementById('myCheck4');
-// button4.addEventListener('click',myFunction(button4));
-// const button5= document.getElementById('myCheck5');
-// button5.addEventListener('click',myFunction(button5));
-
-// function myFunction(checkBox) {
-//   const text = document.getElementById('text');
-//   if (checkBox.checked == true){
-//     text.style.display = "block";
-//   } else {
-//     text.style.display = "none";
-//   }
-// }
-// </script>
-// <script>
-// var wow = new WOW(
-//          {
-//            boxClass:     'wow',      
-//            animateClass: 'animated', 
-//            offset:       0,         
-//            mobile:       true,      
-//            live:         true,       
-//            callback:     function(box) {
-//            },
-//            scrollContainer: null 
-//          }
-//        );
-//        wow.init();
-// </script>
+// var buttonsubmit= document.getElementById('form');
+// buttonsubmit.addEventListener('submit', ()=>{
+//     buttonsubmit.submit();
+//     buttonsubmit.reset();
+//     return false;
+// })
